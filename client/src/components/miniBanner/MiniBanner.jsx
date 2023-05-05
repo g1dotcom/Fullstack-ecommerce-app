82336c9e790cf9800e2bbb37b70b90ee62d95067
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-05 19:56:45 +0300

    shadow ve birkaç css özelliği eklendi

--- client/src/components/miniBanner/MiniBanner.jsx
+++ client/src/components/miniBanner/MiniBanner.jsx
@@ -12,25 +12,25 @@ import img7 from "./images/32.png";
 const MiniBanner = () => {
   return (
     <div className="my-10 relative flex flex-wrap justify-around">
-      <div className="my-10">
-        <img className="w-[400px] rounded-lg " src={img1} alt="" />
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-orange-700 shadow-black transition-all duration-300 ">
+        <img className="w-[400px] rounded-lg  " src={img1} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-blue-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img5} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-pink-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img2} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-purple-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img6} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-red-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img3} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-yellow-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img7} alt="" />
       </div>
-      <div className="my-10">
+      <div className="my-10 cursor-pointer hover:scale-110 rounded-lg shadow-2xl hover:shadow-blue-700 shadow-black transition-all duration-300">
         <img className="w-[400px] rounded-lg " src={img4} alt="" />
       </div>
     </div>
