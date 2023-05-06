20fc763a80cf02467e65c101b00549be831e52b7
Gökhan Süle <thegkhnsl@gmail.com>
2023-05-06 16:24:58 +0300

    Banner1jsx gölge ve hover özellikleri eklendi

--- client/src/components/servicesC/right/rightComp/Banner3.jsx
+++ client/src/components/servicesC/right/rightComp/Banner3.jsx
@@ -16,18 +16,19 @@ import img2 from "../../images/k2.png";
 
 const Banner3 = () => {
   return (
-    <div className="flex justify-center items-center bg-white  h-full w-full mx-10 ">
+    <div
+      className="flex cursor-pointer justify-center items-center bg-white  h-full w-full mx-10 
+    hover:shadow-2xl hover:shadow-blue-400 hover:scale-90 hover:text-white hover:bg-gray-800 transition-all duration-300"
+    >
       <div className="flex  justify-evenly w-full items-center ">
-        <h1 className="font-bold text-2xl text-gray-500">
-          Tüm Kulaklık Çeşitleri
-        </h1>
+        <h1 className="font-bold text-2xl ">Tüm Kulaklık Çeşitleri</h1>
         <div className="flex justify-evenly w-1/2 items-center bg-gray-100">
           <div className="flex flex-col  ">
             {" "}
-            <span className="flex flex-col justify-center items-center my-8 hover:scale-125 transition-all duration-300">
-              <h1 className="font-bold">Apple</h1>
+            <span className="flex flex-col justify-center items-center my-8 hover:scale-125 transition-all duration-400">
+              <h1 className="font-bold text-black">Apple</h1>
               <p>
-                <AiFillApple size={48} />
+                <AiFillApple className="text-black" size={48} />
               </p>
             </span>
             <span className="flex  justify-center items-center my-8">
@@ -44,7 +45,7 @@ const Banner3 = () => {
             </span>
             <span className="flex flex-col justify-center items-center my-8 hover:scale-125 transition-all duration-300">
               <p>
-                <SiSamsung size={64} />
+                <SiSamsung className="text-black" size={64} />
               </p>
             </span>
           </div>
